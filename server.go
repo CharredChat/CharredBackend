0e3765b35f1e6256a89895b8a5ad74ec5b4302b3
endercass <casimirrichardson123@gmail.com>
2023-04-26 01:09:01 -0400

    echo server test

--- server.go
+++ server.go
@@ -3,8 +3,11 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 
 	"github.com/CharredChat/charrid/CharredProcess"
+	"github.com/gobwas/ws"
+	"github.com/gobwas/ws/wsutil"
 )
 
 func main() {
@@ -16,4 +19,31 @@ func main() {
 		return
 	}
 	fmt.Printf("ID: %X\n", test.AsUint64())
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		conn, _, _, err := ws.UpgradeHTTP(r, w)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+		go func() {
+			defer conn.Close()
+
+			for {
+				msg, op, err := wsutil.ReadClientData(conn)
+				if err != nil {
+					// handle error
+					log.Fatal(err)
+					return
+				}
+				err = wsutil.WriteServerMessage(conn, op, msg)
+				if err != nil {
+					// handle error
+					log.Fatal(err)
+					return
+				}
+			}
+		}()
+	})
+	http.ListenAndServe("127.0.0.1:8080", nil)
+
 }
